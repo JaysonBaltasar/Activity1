d918492edf20820ec0b3ed5cf663b6ec1f6c5bca
JaysonBaltasar <148601774+JaysonBaltasar@users.noreply.github.com>
2024-02-07 22:03:47 +0800

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+header
+
+import '../App.css'
+
+function header(){
+    return(
+        <div className='header'>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default header;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Son from './Components/Son';
+import Header from './Components/Header';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Son />
+    <Header />
+  </React.StrictMode>,
+)
